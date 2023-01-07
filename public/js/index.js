f86ced5540d41d8053b78c7a440c560e333745f3
Błażej Domagała <petitoff@outlook.com>
2023-01-07 02:10:00 +0100

    Updating User Password with Our API

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,17 @@
-/* eslint-disable node/no-unsupported-features/es-syntax */
-/* eslint-disable no-undef */
+/* eslint-disable */
 import '@babel/polyfill';
-import { login, logout } from './login';
 import { displayMap } from './mapbox';
+import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.form--login');
 
-const logOutButton = document.querySelector('.nav__el--logout');
+const loginForm = document.querySelector('.form--login');
+const logOutBtn = document.querySelector('.nav__el--logout');
 
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 // DELEGATION
 if (mapBox) {
@@ -19,30 +19,39 @@ if (mapBox) {
   displayMap(locations);
 }
 
-if (loginForm) {
-  document.querySelector('.form').addEventListener('submit', e => {
+if (loginForm)
+  loginForm.addEventListener('submit', e => {
     e.preventDefault();
-
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
-
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
     login(email, password);
   });
-}
 
-if (logOutButton) {
-  logOutButton.addEventListener('click', () => {
-    logout();
-  });
-}
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
-if (userDataForm) {
+if (userDataForm)
   userDataForm.addEventListener('submit', e => {
     e.preventDefault();
-
-    const email = document.querySelector('#email').value;
-    const name = document.querySelector('#name').value;
-
-    updateSettings({ email, name }, 'data');
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({ name, email }, 'data');
   });
-}
+
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
